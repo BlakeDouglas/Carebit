9866d0843d546f42f69a33bd26d38fcafc4b1bf4
Masspwnser <74734807+Masspwnser@users.noreply.github.com>
2022-09-14 14:24:16 -0400

    Data pulling improvements

--- src/screens/GiverHomeScreen.js
+++ src/screens/GiverHomeScreen.js
@@ -18,17 +18,16 @@ let date = moment().format("dddd, MMM D");
 
 export default function GiverHomeScreen({ navigation }) {
   const [steps, setSteps] = useState(0);
-  const [heart, setHeart] = useState(null);
+  const [HeartBPM, setHeart] = useState(0);
   const [HeartMax, setHeartMax] = useState(0);
-  const [HeartMin, setHearMin] = useState(0);
+  const [HeartMin, setHeartMin] = useState(0);
   const [HeartAvg, setHeartAvg] = useState(0);
   const [fitbitAccessToken, setFitbitAccessToken] = useState(null);
   const userData = useSelector((state) => state.Reducers.userData);
   const tokenData = useSelector((state) => state.Reducers.tokenData);
   const selectedID = 0;
 
-  const refreshFitbitAccessToken = async () => {
-    const caregiveeID = tokenData.caregiveeID[selectedID].caregiveeID;
+  const refreshFitbitAccessToken = async (caregiveeID) => {
     try {
       const response = await fetch(
         "https://www.carebit.xyz/refreshFitbitToken/" + caregiveeID,
@@ -43,16 +42,13 @@ export default function GiverHomeScreen({ navigation }) {
       );
       const json = await response.json();
       if (json.access_token) setFitbitAccessToken(json.access_token);
-      else console.log(json.error);
+      else console.log("Refreshing error: " + json.error);
     } catch (error) {
       console.log("Caught error: " + error);
     }
   };
-  const fetchFitbitAccessToken = async () => {
+  const fetchFitbitAccessToken = async (caregiveeID) => {
     try {
-      console.log(tokenData);
-      const caregiveeID = tokenData.caregiveeID[selectedID].caregiveeID;
-
       const response = await fetch(
         "https://www.carebit.xyz/getFitbitToken/" + caregiveeID,
         {
@@ -65,25 +61,26 @@ export default function GiverHomeScreen({ navigation }) {
         }
       );
       const json = await response.json();
-      setFitbitAccessToken(json.fitbitToken);
+      if (!json.error)
+        setFitbitAccessToken(json.fitbitToken);
+      else
+        console.log("Error: " + json.error);
     } catch (error) {
       console.log("Caught error: " + error);
     }
   };
 
   const fetchData = async () => {
+    const caregiveeID = tokenData.caregiveeID[selectedID].caregiveeID;
     if (!fitbitAccessToken) {
       // Seems that refresh has a cooldown. Switch this on if u get invalid token
       // await refreshFitbitAccessToken();
-
-      await fetchFitbitAccessToken();
+      await fetchFitbitAccessToken(caregiveeID);
     } else {
       let date_today = moment().format("YYYY[-]MM[-]DD");
       //Get HeartRate
       let heartResponse = await fetch(
-        "https://api.fitbit.com/1/user/-/activities/heart/date/" +
-          date_today +
-          "/1d.json",
+        "https://api.fitbit.com/1/user/" + caregiveeID + "/activities/heart/date/" + date_today + "/1d.json",
         {
           headers: {
             Accept: "application/json",
@@ -93,21 +90,23 @@ export default function GiverHomeScreen({ navigation }) {
         }
       );
       let heart = await heartResponse.json();
+
+
+      // Checks for expired token
       if (heart.errors) {
-        console.log(
-          "Error fetching heart. Refresh is recommended, but first confirm."
-        );
-        // await refreshFitbitAccessToken();
+        console.log("Refreshing ");
+        await refreshFitbitAccessToken(caregiveeID);
         return;
       }
-      console.log("Response from heart:");
-      console.log(heart);
+      
+      setHeartAvg(heart["activities-heart"][0].value.restingHeartRate);
+      setHeartMax(heart["activities-heart"][0].value.heartRateZones[3].max);
+      setHeartMin(heart["activities-heart"][0].value.heartRateZones[0].min);
+
 
       //Get Steps
       let stepsResponse = await fetch(
-        "https://api.fitbit.com/1/user/-/activities/date/" +
-          date_today +
-          ".json",
+        "https://api.fitbit.com/1/user/" + caregiveeID + "/activities/tracker/steps/date/" + date_today + "/1d.json",
         {
           headers: {
             Accept: "application/json",
@@ -117,9 +116,8 @@ export default function GiverHomeScreen({ navigation }) {
         }
       );
       let json = await stepsResponse.json();
-      console.log("Response from activities:");
-      console.log(json);
-
+      setSteps(json["activities-tracker-steps"][0].value);
+      console.log("Steps: " + steps);
       //Get Battery
       //TODO: implement levels for different battery icons. getBatteryIcon(level)
       let deviceResponse = await fetch(
@@ -342,7 +340,7 @@ export default function GiverHomeScreen({ navigation }) {
                   fontWeight: "700",
                 }}
               >
-                {heart}
+                {HeartBPM}
               </Text>
               <Text
                 style={{
@@ -407,7 +405,7 @@ export default function GiverHomeScreen({ navigation }) {
                   fontWeight: "700",
                 }}
               >
-                {steps}
+                N/A
               </Text>
             </SafeAreaView>
             <SafeAreaView
