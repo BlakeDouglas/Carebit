7b8aace0a8c4a9a91bc7c10a659ff31df2ce4748
Blake Douglas <93741410+BlakeDouglas@users.noreply.github.com>
2022-09-19 17:16:46 -0400

    Drop down menus for all custom settings

--- src/screens/CustomNotificationScreen.js
+++ src/screens/CustomNotificationScreen.js
@@ -5,12 +5,12 @@ import {
   Switch,
   TextInput,
   StatusBar,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import SelectDropdown from "react-native-select-dropdown";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 export default function CustomNotificationScreen({ navigation }) {
   const [isCustom, setIsCustom] = useState(false);
   const [isHrAlerts, setIsHrAlerts] = useState(true);
@@ -37,7 +37,7 @@ export default function CustomNotificationScreen({ navigation }) {
     setIsBattery(!isBattery);
   };
 
-  const heartLimits = [
+  const lowHeartLimits = [
     "25",
     "26",
     "27",
@@ -105,6 +105,126 @@ export default function CustomNotificationScreen({ navigation }) {
     "89",
     "90",
   ];
+  const highHeartLimits = [
+    "90",
+    "91",
+    "92",
+    "93",
+    "94",
+    "95",
+    "96",
+    "97",
+    "98",
+    "99",
+    "100",
+    "101",
+    "102",
+    "103",
+    "104",
+    "105",
+    "106",
+    "107",
+    "108",
+    "109",
+    "110",
+    "111",
+    "112",
+    "113",
+    "114",
+    "115",
+    "116",
+    "117",
+    "118",
+    "119",
+    "120",
+    "121",
+    "122",
+    "123",
+    "124",
+    "125",
+    "126",
+    "127",
+    "128",
+    "129",
+    "130",
+    "131",
+    "132",
+    "133",
+    "134",
+    "135",
+    "136",
+    "137",
+    "138",
+    "139",
+    "140",
+    "141",
+    "142",
+    "143",
+    "144",
+    "145",
+    "146",
+    "147",
+    "148",
+    "149",
+    "150",
+  ];
+  const noActivityLimit = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+  ];
+
+  const maxSteps = [
+    "500",
+    "750",
+    "1000",
+    "1250",
+    "1000",
+    "1250",
+    "1500",
+    "1750",
+    "2000",
+    "2250",
+    "2500",
+    "2750",
+    "3000",
+    "3250",
+    "3500",
+    "4000",
+    "4500",
+    "5000",
+    "5500",
+    "6000",
+    "6500",
+    "7000",
+    "7500",
+    "8000",
+    "8500",
+    "9000",
+    "9500",
+    "10000",
+  ];
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar
@@ -112,7 +232,7 @@ export default function CustomNotificationScreen({ navigation }) {
         translucent={false}
         backgroundColor="dodgerblue"
       />
-      <KeyboardAwareScrollView style={{ height: "100%", width: "100%" }}>
+      <ScrollView>
         <SafeAreaView style={[styles.Box, { marginTop: "3%" }]}>
           <Text style={[styles.Title, { margin: "4%" }]}>
             Use Custom Alerts
@@ -128,8 +248,8 @@ export default function CustomNotificationScreen({ navigation }) {
         <SafeAreaView>
           <Text style={styles.Descriptive}>
             {isCustom
-              ? "Turn off to use custom thresholds instead of Activity Levels to trigger alerts"
-              : "Turn on to use Activity Levels instead of custom thresholds to trigger alerts"}
+              ? "Turn off to use Activity Levels instead of Custom Thresholds for notifications"
+              : "Turn on to use Custom Thresholds instead of Activity Levels for notifications"}
           </Text>
         </SafeAreaView>
         {isCustom ? (
@@ -159,20 +279,21 @@ export default function CustomNotificationScreen({ navigation }) {
                 );
               }}
               dropdownIconPosition={"right"}
-              defaultButtonText={"0 BPM"}
+              defaultValue={"50"}
               disableAutoScroll={true}
+              //search={true}
               selectedRowStyle={{ backgroundColor: "lightgray" }}
               buttonStyle={styles.downButtonStyle}
               buttonTextStyle={{
                 color: "rgba(128,128,128,.9)",
                 fontSize: responsiveFontSize(2.2),
               }}
-              data={heartLimits}
+              data={lowHeartLimits}
               onSelect={(selectedItem, index) => {
                 console.log(selectedItem, index);
               }}
               buttonTextAfterSelection={(selectedItem, index) => {
-                return selectedItem + " BPM";
+                return selectedItem + " bpm";
               }}
               rowTextForSelection={(item, index) => {
                 return item;
@@ -180,18 +301,42 @@ export default function CustomNotificationScreen({ navigation }) {
             />
           </SafeAreaView>
         ) : null}
-        {isHrAlerts && isCustom ? (
+        {isHrAlerts && isCustom && (
           <SafeAreaView style={styles.Box}>
             <Text style={styles.Title}>High Heart Rate</Text>
-            <TextInput
-              style={[styles.Inputs]}
-              placeholderTextColor={"rgba(128,128,128,.9)"}
-              placeholder="150 BPM"
-              keyboardType="number-pad"
-            ></TextInput>
+            <SelectDropdown
+              renderDropdownIcon={(isOpened) => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? "chevron-up" : "chevron-down"}
+                    color={"rgba(128,128,128,.7)"}
+                    size={15}
+                  />
+                );
+              }}
+              dropdownIconPosition={"right"}
+              defaultValue={"110"}
+              disableAutoScroll={true}
+              selectedRowStyle={{ backgroundColor: "lightgray" }}
+              buttonStyle={styles.downButtonStyle}
+              buttonTextStyle={{
+                color: "rgba(128,128,128,.9)",
+                fontSize: responsiveFontSize(2.2),
+              }}
+              data={highHeartLimits}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem + " bpm";
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+            />
           </SafeAreaView>
-        ) : null}
-        {isCustom ? (
+        )}
+        {isCustom && (
           <SafeAreaView style={[styles.Box, { marginTop: "10%" }]}>
             <Text style={styles.Title}>No Activity Alerts</Text>
             <Switch
@@ -202,31 +347,83 @@ export default function CustomNotificationScreen({ navigation }) {
               value={isActivityAlerts}
             />
           </SafeAreaView>
-        ) : null}
-        {isActivityAlerts && isCustom ? (
+        )}
+        {isActivityAlerts && isCustom && (
           <SafeAreaView style={styles.Box}>
             <Text style={styles.Title}>Time Without Heart Rate</Text>
-            <TextInput
-              style={[styles.Inputs]}
-              placeholderTextColor={"rgba(128,128,128,.9)"}
-              placeholder="1 hour"
-              keyboardType="number-pad"
-            ></TextInput>
+            <SelectDropdown
+              renderDropdownIcon={(isOpened) => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? "chevron-up" : "chevron-down"}
+                    color={"rgba(128,128,128,.7)"}
+                    size={15}
+                  />
+                );
+              }}
+              dropdownIconPosition={"right"}
+              defaultValue={"1"}
+              disableAutoScroll={true}
+              selectedRowStyle={{ backgroundColor: "lightgray" }}
+              buttonStyle={styles.downButtonStyle}
+              buttonTextStyle={{
+                color: "rgba(128,128,128,.9)",
+                fontSize: responsiveFontSize(2.2),
+              }}
+              data={noActivityLimit}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem > 1
+                  ? selectedItem + " hours"
+                  : selectedItem + " hour";
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+            />
           </SafeAreaView>
-        ) : null}
-        {isActivityAlerts && isCustom ? (
+        )}
+        {isActivityAlerts && isCustom && (
           <SafeAreaView style={styles.Box}>
             <Text style={styles.Title}>Time Without Steps</Text>
-            <TextInput
-              style={[styles.Inputs]}
-              placeholderTextColor={"rgba(128,128,128,.9)"}
-              placeholder="1 hour"
-              keyboardType="number-pad"
-            ></TextInput>
+            <SelectDropdown
+              renderDropdownIcon={(isOpened) => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? "chevron-up" : "chevron-down"}
+                    color={"rgba(128,128,128,.7)"}
+                    size={15}
+                  />
+                );
+              }}
+              dropdownIconPosition={"right"}
+              defaultValue={"1"}
+              disableAutoScroll={true}
+              selectedRowStyle={{ backgroundColor: "lightgray" }}
+              buttonStyle={styles.downButtonStyle}
+              buttonTextStyle={{
+                color: "rgba(128,128,128,.9)",
+                fontSize: responsiveFontSize(2.2),
+              }}
+              data={noActivityLimit}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem > 1
+                  ? selectedItem + " hours"
+                  : selectedItem + " hour";
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+            />
           </SafeAreaView>
-        ) : null}
+        )}
 
-        {isCustom ? (
+        {isCustom && (
           <SafeAreaView style={[styles.Box, { marginTop: "10%" }]}>
             <Text style={styles.Title}>Wandering Alerts</Text>
             <Switch
@@ -237,19 +434,44 @@ export default function CustomNotificationScreen({ navigation }) {
               value={isWandering}
             />
           </SafeAreaView>
-        ) : null}
-        {isWandering && isCustom ? (
+        )}
+        {isWandering && isCustom && (
           <SafeAreaView style={styles.Box}>
             <Text style={styles.Title}>Max Steps in an Hour</Text>
-            <TextInput
-              style={[styles.Inputs]}
-              placeholderTextColor={"rgba(128,128,128,.9)"}
-              placeholder="1,000 steps"
-              keyboardType="number-pad"
-            ></TextInput>
+            <SelectDropdown
+              renderDropdownIcon={(isOpened) => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? "chevron-up" : "chevron-down"}
+                    color={"rgba(128,128,128,.7)"}
+                    size={15}
+                  />
+                );
+              }}
+              dropdownIconPosition={"right"}
+              defaultValue={"2000"}
+              disableAutoScroll={true}
+              //search={true}
+              selectedRowStyle={{ backgroundColor: "lightgray" }}
+              buttonStyle={styles.downButtonStyle2}
+              buttonTextStyle={{
+                color: "rgba(128,128,128,.9)",
+                fontSize: responsiveFontSize(2.2),
+              }}
+              data={maxSteps}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem + " steps";
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+            />
           </SafeAreaView>
-        ) : null}
-        {isCustom ? (
+        )}
+        {isCustom && (
           <SafeAreaView>
             <SafeAreaView style={[styles.Box, { marginTop: "10%" }]}>
               <Text style={styles.Title}>No Sync Alerts</Text>
@@ -268,11 +490,11 @@ export default function CustomNotificationScreen({ navigation }) {
               </Text>
             </SafeAreaView>
           </SafeAreaView>
-        ) : null}
+        )}
 
-        {isCustom ? (
+        {isCustom && (
           <SafeAreaView>
-            <SafeAreaView style={[styles.Box, { marginTop: "10%" }]}>
+            <SafeAreaView style={[styles.Box, { marginTop: "5%" }]}>
               <Text style={styles.Title}>Empty Battery Alerts</Text>
               <Switch
                 trackColor={{ false: "lightgray", true: "mediumaquamarine" }}
@@ -288,8 +510,8 @@ export default function CustomNotificationScreen({ navigation }) {
               </Text>
             </SafeAreaView>
           </SafeAreaView>
-        ) : null}
-      </KeyboardAwareScrollView>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -327,7 +549,12 @@ const styles = StyleSheet.create({
   },
   downButtonStyle: {
     height: "100%",
-    width: "29.9%",
+    width: "30.7%",
+    backgroundColor: "white",
+  },
+  downButtonStyle2: {
+    height: "100%",
+    width: "38%",
     backgroundColor: "white",
   },
 });
