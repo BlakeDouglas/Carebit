4e37ef6e4dadf2d0bfa8538d45961d812fc1d608
Masspwnser <74734807+Masspwnser@users.noreply.github.com>
2022-10-05 20:53:49 -0400

    selectedUser data fix

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -109,8 +109,7 @@ export default function LoginScreen({ navigation }) {
       const responseText = await response.text();
       const json = JSON.parse(responseText);
 
-      if (json.default && json.default[0])
-        dispatch(setSelectedUser(json.default[0]));
+      if (json.default) dispatch(setSelectedUser(json.default));
       else dispatch(resetSelectedData());
     } catch (error) {
       console.log(
