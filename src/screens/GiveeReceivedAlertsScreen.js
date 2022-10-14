5a3fe9b6eb139cced905b6d1637e0f1cd2f1af9d
Blake Douglas <93741410+BlakeDouglas@users.noreply.github.com>
2022-10-13 21:12:54 -0400

    refresh alerts/empty handling/name corrections/etc

--- src/screens/GiveeReceivedAlertsScreen.js
+++ src/screens/GiveeReceivedAlertsScreen.js
@@ -6,6 +6,7 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  RefreshControl,
 } from "react-native";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
 import React, { useState } from "react";
@@ -14,16 +15,16 @@ import Modal from "react-native-modal";
 
 const data_temp = [
   {
-    alertType: "StepsHigh",
+    alertType: "lowHeartRateAlert",
     dateTime: "12:50pm",
-    title: "Too Many Steps",
-    body: "Name has taken more than 865 steps in the past hour",
+    title: "Low Heart Rate",
+    body: "Name has low heart rate",
     caregiveeID: "BH32L",
     alertID: "1",
     ok: 1,
   },
   {
-    alertType: "StepsNone",
+    alertType: "noStepsAlert",
     dateTime: "1:15am",
     title: "No Steps",
     body: "Name has gone more than x hour(s) without steps",
@@ -32,7 +33,7 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "HeartNone",
+    alertType: "noHeartRateAlert",
     dateTime: "1:18am",
     title: "Heart Rate Not Recorded",
     body: "Name's heart rate hasn't been recorded for over 8 hour(s)",
@@ -41,7 +42,7 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "HeartHigh",
+    alertType: "highHeartRateAlert",
     dateTime: "10:16am",
     title: "High Heart Rate",
     body: "Name's heart rate was 162",
@@ -50,7 +51,7 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "StepsHigh",
+    alertType: "tooManyStepsAlert",
     dateTime: "12:50pm",
     title: "Too Many Steps",
     body: "Name has taken more than 865 steps in the past hour",
@@ -59,25 +60,25 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "StepsNone",
+    alertType: "noSyncAlert",
     dateTime: "1:15am",
-    title: "No Steps",
-    body: "Name has gone more than x hour(s) without steps",
+    title: "No Sync",
+    body: "Name has gone more than x hour(s) without syncing",
     total: "0",
     alertID: "6",
     ok: 1,
   },
   {
-    alertType: "HeartNone",
+    alertType: "batteryAlert",
     dateTime: "8:16am",
-    title: "Heart Rate Not Recorded",
-    body: "Name's heart rate hasn't been recorded for over 8 hour(s)",
+    title: "Battery Warning",
+    body: "Their Fitbit is dead",
     total: "8",
     alertID: "7",
     ok: 0,
   },
   {
-    alertType: "HeartLow",
+    alertType: "lowHeartRateAlert",
     dateTime: "10:16am",
     title: "Low Heart Rate",
     body: "Name's heart rate was 62",
@@ -89,7 +90,7 @@ const data_temp = [
 
 export default function GiveeReceivedAlertsScreen({ navigation }) {
   const tokenData = useSelector((state) => state.Reducers.tokenData);
-
+  const [data, setData] = useState([]);
   const getAlerts = async () => {
     try {
       const response = await fetch(
@@ -105,7 +106,8 @@ export default function GiveeReceivedAlertsScreen({ navigation }) {
       );
       const json = await response.json();
       if (json) {
-        console.log(json.alerts);
+        setData(json.alerts);
+        console.log(data);
       }
     } catch (error) {
       console.log(
@@ -187,7 +189,7 @@ export default function GiveeReceivedAlertsScreen({ navigation }) {
                 alignItems: "center",
                 justifyContent: "center",
                 borderTopColor: "rgba(128, 128, 128, .2)",
-                borderTopWalertIDth: 1,
+                borderTopwidth: 1,
               }}
             >
               <TouchableOpacity
@@ -217,7 +219,7 @@ export default function GiveeReceivedAlertsScreen({ navigation }) {
                 alignItems: "center",
                 justifyContent: "center",
                 borderTopColor: "rgba(128, 128, 128, .2)",
-                borderTopWalertIDth: 1,
+                borderTopwidth: 1,
               }}
             >
               <TouchableOpacity
@@ -246,39 +248,54 @@ export default function GiveeReceivedAlertsScreen({ navigation }) {
           marginVertical: "1%",
           flexDirection: "row",
           alignItems: "center",
+          borderTopColor: "lightgrey",
+          borderTopWidth: 1,
           //backgroundColor: "red",
         }}
       >
-        {alertType === "StepsHigh" && (
+        {alertType === "tooManyStepsAlert" && (
           <Image
             style={styles.images}
             source={require("../../assets/images/icons-alert-steps-high.imageset/icons-alert-steps-high.png")}
           />
         )}
-        {alertType === "StepsNone" && (
+        {alertType === "noStepsAlert" && (
           <Image
             style={styles.images}
             source={require("../../assets/images/icons-alert-steps-none.imageset/icons-alert-steps-none.png")}
           />
         )}
-        {alertType === "HeartHigh" && (
+        {alertType === "highHeartRateAlert" && (
           <Image
             style={styles.images}
             source={require("../../assets/images/icons-alert-heart-high.imageset/icons-alert-heart-high.png")}
           />
         )}
-        {alertType === "HeartLow" && (
+        {alertType === "lowHeartRateAlert" && (
           <Image
             style={styles.images}
             source={require("../../assets/images/icons-alert-heart-low.imageset/icons-alert-heart-low.png")}
           />
         )}
-        {alertType === "HeartNone" && (
+        {alertType === "noHeartRateAlert" && (
           <Image
             style={styles.images}
             source={require("../../assets/images/icons-alert-heart-none.imageset/icons-alert-heart-none.png")}
           />
         )}
+        {alertType === "noSyncAlert" && (
+          <Image
+            style={styles.images}
+            source={require("../../assets/images/icons-alert-sync.imageset/icons-alert-sync.png")}
+          />
+        )}
+        {alertType === "batteryAlert" && (
+          <Image
+            style={styles.images}
+            source={require("../../assets/images/icons-alert-battery.imageset/icons-alert-battery.png")}
+          />
+        )}
+
         <SafeAreaView
           style={{
             marginLeft: "3%",
@@ -365,6 +382,26 @@ export default function GiveeReceivedAlertsScreen({ navigation }) {
       ok={item.ok}
     />
   );
+
+  const Empty = () => {
+    return (
+      <View style={styles.emptyContainer}>
+        <Text style={styles.emptyText}>No alerts</Text>
+        <Text style={styles.emptyText}>...</Text>
+      </View>
+    );
+  };
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    getAlerts();
+    wait(1000).then(() => setRefreshing(false));
+  }, []);
+
   return (
     <SafeAreaView
       style={{
@@ -389,8 +426,12 @@ export default function GiveeReceivedAlertsScreen({ navigation }) {
         </Text>
       </SafeAreaView>
       <FlatList
-        data={data_temp}
+        data={data}
         renderItem={renderItem}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        ListEmptyComponent={Empty}
         keyExtractor={(item) => item.alertID}
         //backgroundColor="blue"
       />
@@ -402,5 +443,15 @@ const styles = StyleSheet.create({
   images: {
     height: 50,
     width: 50,
+  },
+  emptyText: {
+    color: "black",
+    fontSize: responsiveFontSize(3.5),
+    alignSelf: "center",
+  },
+  emptyContainer: {
+    paddingTop: "5%",
+    borderTopColor: "lightgrey",
+    borderTopWidth: 1,
   },
 });
--- src/screens/ReceivedAlertsScreen.js
+++ src/screens/ReceivedAlertsScreen.js
@@ -9,24 +9,27 @@ import {
   View,
   Image,
   FlatList,
+  RefreshControl,
 } from "react-native";
 import moment from "moment";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
 //const selectedUser = useSelector((state) => state.Reducers.selectedUser);
 import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const data_temp = [
   {
-    alertType: "StepsHigh",
+    alertType: "lowHeartRateAlert",
     dateTime: "12:50pm",
-    title: "Too Many Steps",
-    body: "Name has taken more than 865 steps in the past hour",
+    title: "Low Heart Rate",
+    body: "Name has low heart rate",
     caregiveeID: "BH32L",
     alertID: "1",
     ok: 1,
   },
   {
-    alertType: "StepsNone",
+    alertType: "noStepsAlert",
     dateTime: "1:15am",
     title: "No Steps",
     body: "Name has gone more than x hour(s) without steps",
@@ -35,7 +38,7 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "HeartNone",
+    alertType: "noHeartRateAlert",
     dateTime: "1:18am",
     title: "Heart Rate Not Recorded",
     body: "Name's heart rate hasn't been recorded for over 8 hour(s)",
@@ -44,7 +47,7 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "HeartHigh",
+    alertType: "highHeartRateAlert",
     dateTime: "10:16am",
     title: "High Heart Rate",
     body: "Name's heart rate was 162",
@@ -53,7 +56,7 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "StepsHigh",
+    alertType: "tooManyStepsAlert",
     dateTime: "12:50pm",
     title: "Too Many Steps",
     body: "Name has taken more than 865 steps in the past hour",
@@ -62,25 +65,25 @@ const data_temp = [
     ok: 0,
   },
   {
-    alertType: "StepsNone",
+    alertType: "noSyncAlert",
     dateTime: "1:15am",
-    title: "No Steps",
-    body: "Name has gone more than x hour(s) without steps",
+    title: "No Sync",
+    body: "Name has gone more than x hour(s) without syncing",
     total: "0",
     alertID: "6",
     ok: 1,
   },
   {
-    alertType: "HeartNone",
+    alertType: "batteryAlert",
     dateTime: "8:16am",
-    title: "Heart Rate Not Recorded",
-    body: "Name's heart rate hasn't been recorded for over 8 hour(s)",
+    title: "Battery Warning",
+    body: "Their Fitbit is dead",
     total: "8",
     alertID: "7",
     ok: 0,
   },
   {
-    alertType: "HeartLow",
+    alertType: "lowHeartRateAlert",
     dateTime: "10:16am",
     title: "Low Heart Rate",
     body: "Name's heart rate was 62",
@@ -89,98 +92,118 @@ const data_temp = [
     ok: 0,
   },
 ];
-
-const Item = ({ alertType, dateTime, body, title, ok }) => (
-  <SafeAreaView
-    style={{
-      marginVertical: "1%",
-      flexDirection: "row",
-      alignItems: "center",
-      //backgroundColor: "red",
-    }}
-  >
-    {alertType === "StepsHigh" && (
-      <Image
-        style={styles.images}
-        source={require("../../assets/images/icons-alert-steps-high.imageset/icons-alert-steps-high.png")}
-      />
-    )}
-    {alertType === "StepsNone" && (
-      <Image
-        style={styles.images}
-        source={require("../../assets/images/icons-alert-steps-none.imageset/icons-alert-steps-none.png")}
-      />
-    )}
-    {alertType === "HeartHigh" && (
-      <Image
-        style={styles.images}
-        source={require("../../assets/images/icons-alert-heart-high.imageset/icons-alert-heart-high.png")}
-      />
-    )}
-    {alertType === "HeartLow" && (
-      <Image
-        style={styles.images}
-        source={require("../../assets/images/icons-alert-heart-low.imageset/icons-alert-heart-low.png")}
-      />
-    )}
-    {alertType === "HeartNone" && (
-      <Image
-        style={styles.images}
-        source={require("../../assets/images/icons-alert-heart-none.imageset/icons-alert-heart-none.png")}
-      />
-    )}
-    <SafeAreaView
-      style={{
-        marginLeft: "3%",
-        //height: "100%",
-        width: "58%",
-        //backgroundColor: "blue",
-        marginVertical: "5%",
-        justifyContent: "center",
-      }}
-    >
-      <Text style={{ fontSize: responsiveFontSize(2.4), fontWeight: "600" }}>
-        {title}
-      </Text>
-      <Text
-        style={{
-          marginTop: "1%",
-          fontSize: responsiveFontSize(1.8),
-          color: "gray",
-        }}
-        numberOfLines={2}
-      >
-        {body}
-      </Text>
-    </SafeAreaView>
-    <SafeAreaView
-      style={{
-        marginLeft: "1%",
-        marginRight: "3%",
-        // backgroundColor: "red",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <Text
-        style={{
-          color: "rgba(0,225,200,.6)",
-          fontWeight: "bold",
-          textAlign: "right",
-          fontSize: responsiveFontSize(2.3),
-          marginTop: "10%",
-        }}
-      >
-        OKAY
-      </Text>
-      <Text style={{ color: "grey", marginTop: "14%" }}>{dateTime}</Text>
-    </SafeAreaView>
-  </SafeAreaView>
-);
-
 export default function ReceivedAlertsScreen({ navigation }) {
+  const Item = ({ alertType, dateTime, body, title, ok }) => (
+    <SafeAreaView
+      style={{
+        marginVertical: "1%",
+        flexDirection: "row",
+        alignItems: "center",
+        borderTopColor: "lightgrey",
+        borderTopWidth: 1,
+        //backgroundColor: "red",
+      }}
+    >
+      {alertType === "tooManyStepsAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-steps-high.imageset/icons-alert-steps-high.png")}
+        />
+      )}
+      {alertType === "noStepsAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-steps-none.imageset/icons-alert-steps-none.png")}
+        />
+      )}
+      {alertType === "highHeartRateAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-heart-high.imageset/icons-alert-heart-high.png")}
+        />
+      )}
+      {alertType === "lowHeartRateAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-heart-low.imageset/icons-alert-heart-low.png")}
+        />
+      )}
+      {alertType === "noHeartRateAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-heart-none.imageset/icons-alert-heart-none.png")}
+        />
+      )}
+      {alertType === "noSyncAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-sync.imageset/icons-alert-sync.png")}
+        />
+      )}
+      {alertType === "batteryAlert" && (
+        <Image
+          style={styles.images}
+          source={require("../../assets/images/icons-alert-battery.imageset/icons-alert-battery.png")}
+        />
+      )}
+      <SafeAreaView
+        style={{
+          marginLeft: "3%",
+          //height: "100%",
+          width: "58%",
+          //backgroundColor: "blue",
+          marginVertical: "5%",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontSize: responsiveFontSize(2.4), fontWeight: "600" }}>
+          {title}
+        </Text>
+        <Text
+          style={{
+            marginTop: "1%",
+            fontSize: responsiveFontSize(1.8),
+            color: "gray",
+          }}
+          numberOfLines={2}
+        >
+          {body}
+        </Text>
+      </SafeAreaView>
+      <SafeAreaView
+        style={{
+          marginLeft: "1%",
+          marginRight: "3%",
+          // backgroundColor: "red",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            // See json return
+            getAlerts();
+          }}
+        >
+          <Text
+            style={{
+              color: "rgba(0,225,200,.6)",
+              fontWeight: "bold",
+              textAlign: "right",
+              fontSize: responsiveFontSize(2.3),
+              marginTop: "10%",
+            }}
+          >
+            OKAY
+          </Text>
+        </TouchableOpacity>
+        <Text style={{ color: "grey", marginTop: "14%" }}>{dateTime}</Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
+
   //const [data, setData] = useState([]);
   const tokenData = useSelector((state) => state.Reducers.tokenData);
-
+  const [data, setData] = useState([]);
   const getAlerts = async () => {
     try {
       const response = await fetch(
@@ -196,7 +219,8 @@ export default function ReceivedAlertsScreen({ navigation }) {
       );
       const json = await response.json();
       if (json) {
-        console.log(json.alerts);
+        setData(json.alerts);
+        console.log(data);
       }
     } catch (error) {
       console.log(
@@ -214,6 +238,26 @@ export default function ReceivedAlertsScreen({ navigation }) {
       ok={item.ok}
     />
   );
+
+  const Empty = () => {
+    return (
+      <View style={styles.emptyContainer}>
+        <Text style={styles.emptyText}>No alerts</Text>
+        <Text style={styles.emptyText}>...</Text>
+      </View>
+    );
+  };
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    getAlerts();
+    wait(1000).then(() => setRefreshing(false));
+  }, []);
+
   return (
     <SafeAreaView
       style={{
@@ -240,7 +284,11 @@ export default function ReceivedAlertsScreen({ navigation }) {
       <FlatList
         data={data_temp}
         renderItem={renderItem}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         keyExtractor={(item) => item.alertID}
+        ListEmptyComponent={Empty}
         //backgroundColor="blue"
       />
     </SafeAreaView>
@@ -251,5 +299,15 @@ const styles = StyleSheet.create({
   images: {
     height: 50,
     width: 50,
+  },
+  emptyText: {
+    color: "black",
+    fontSize: responsiveFontSize(3.5),
+    alignSelf: "center",
+  },
+  emptyContainer: {
+    paddingTop: "5%",
+    borderTopColor: "lightgrey",
+    borderTopWidth: 1,
   },
 });
