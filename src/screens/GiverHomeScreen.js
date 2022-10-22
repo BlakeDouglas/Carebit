d40000bba393134655f870789ad8e9c9996dde9f
Blake Douglas <93741410+BlakeDouglas@users.noreply.github.com>
2022-10-21 21:32:28 -0400

    small text fix

--- src/screens/GiverHomeScreen.js
+++ src/screens/GiverHomeScreen.js
@@ -853,7 +853,7 @@ export default function GiverHomeScreen({ navigation }) {
                     justifyContent: "flex-start",
                   }}
                 >
-                  <Text style={styles.smallText}>{StepsSyncTime || ""}</Text>
+                  <Text style={styles.smallText}>in past hour</Text>
                 </SafeAreaView>
               </SafeAreaView>
             </SafeAreaView>
