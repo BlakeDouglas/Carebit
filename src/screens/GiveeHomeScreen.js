d50966cf16e8f0265680e58d6b31b64f0e50e18f
jizquiec <105866519+jizquiec@users.noreply.github.com>
2022-11-10 23:50:26 -0500

    get and store notificationtoken for request notification
    Signed-off-by: jizquiec <105866519+jizquiec@users.noreply.github.com>

--- src/screens/GiveeHomeScreen.js
+++ src/screens/GiveeHomeScreen.js
@@ -20,10 +20,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
 import Modal from "react-native-modal";
 import call from "react-native-phone-call";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import { resetSelectedData, setSelectedUser } from "../redux/actions";
 import {
   caregiveeGetEndpoint,
   caregiveeSetEndpoint,
+  notificationTokenEndpoint,
   getDefaultEndpoint,
   fitbitDataEndpoint,
   alertCounter,
@@ -124,6 +127,50 @@ export default function GiveeHomeScreen({ navigation }) {
     if (json.default) {
       dispatch(setSelectedUser(json.default));
     }
+  };
+
+  // Get Device expo-token-Notification
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        console.log("Failed to get push token for push notification!");
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      storeMessageToken(token);
+    } else {
+      console.log("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    return token;
+  }
+
+  // Stores expo-token-notification in user's database
+  const storeMessageToken = async (token) => {
+    const params = {
+      payload: token,
+      selfID: tokenData.userID,
+      auth: tokenData.access_token,
+    };
+
+    const json = await notificationTokenEndpoint(params);
+    // TODO: Implement error catching here
   };
 
   // Receive a count of how many alerts are present
@@ -244,6 +291,11 @@ export default function GiveeHomeScreen({ navigation }) {
     fetchData();
     getAlertCounter();
   }, [selectedUser]);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+
+  }, []);
 
   const { fontScale } = useWindowDimensions();
   return (
